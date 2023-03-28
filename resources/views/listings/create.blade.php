9ac6ace880bad70214e829649f5862ac8c46916a
Nick Jackson <nickdjackson@msn.com>
2023-03-27 20:33:37 -0400

    revisions to form submission

--- resources/views/listings/create.blade.php
+++ resources/views/listings/create.blade.php
@@ -22,6 +22,9 @@
             class="border border-gray-200 rounded p-2 w-full"
             name="company"
         />
+        @error('company')
+            <p class="text-red-500 text-us mt-1">{{ $message }}</p>
+        @enderror
     </div>
 
     <div class="mb-6">
@@ -34,6 +37,9 @@
             name="title"
             placeholder="Example: Senior Laravel Developer"
         />
+        @error('title')
+        <p class="text-red-500 text-us mt-1">{{ $message }}</p>
+    @enderror
     </div>
 
     <div class="mb-6">
@@ -48,6 +54,9 @@
             name="location"
             placeholder="Example: Remote, Boston MA, etc"
         />
+        @error('location')
+        <p class="text-red-500 text-us mt-1">{{ $message }}</p>
+    @enderror
     </div>
 
     <div class="mb-6">
@@ -59,6 +68,9 @@
             class="border border-gray-200 rounded p-2 w-full"
             name="email"
         />
+        @error('email')
+        <p class="text-red-500 text-us mt-1">{{ $message }}</p>
+    @enderror
     </div>
 
     <div class="mb-6">
@@ -73,6 +85,9 @@
             class="border border-gray-200 rounded p-2 w-full"
             name="website"
         />
+        @error('website')
+        <p class="text-red-500 text-us mt-1">{{ $message }}</p>
+    @enderror
     </div>
 
     <div class="mb-6">
@@ -85,6 +100,9 @@
             name="tags"
             placeholder="Example: Laravel, Backend, Postgres, etc"
         />
+        @error('tags')
+        <p class="text-red-500 text-us mt-1">{{ $message }}</p>
+    @enderror
     </div>
 
     {{-- <div class="mb-6">
@@ -111,6 +129,9 @@
             rows="10"
             placeholder="Include tasks, requirements, salary, etc"
         ></textarea>
+        @error('description')
+        <p class="text-red-500 text-us mt-1">{{ $message }}</p>
+    @enderror
     </div>
 
     <div class="mb-6">
